d42fa43ad4409cf6fe15a56a8b9d5ec8681ce6e1
Francisco Carrera <franciscocarrer@gmail.com>
2017-11-01 20:42:14 -0400

    Added mongoose controller for sites

---
+++ controllers/siteController.js
@@ -0,0 +1,25 @@
+const db = require("../models");
+
+// Defining methods for the SitesController
+module.exports = {
+  findAll: function(req, res) {
+    db.Site
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Site
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Site
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
